528471320b2d42dc97994821d778f72e6a21ef81
DomuStyle <oxgameboyxo@googlemail.com>
2024-11-28 12:33:09 +0100

    added: getConetntTemplate(), fetch.js + templates.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 async function fetchDataJson() {
-    let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=15&offset=0');
     let responseAsJson = await response.json();
     console.log(responseAsJson);
+
+    let htmlcontent = "";
+    for (let index = 0; index < responseAsJson.results.length; index++) {
+        htmlcontent += getContentTemplate(responseAsJson, index);
+    }
+
+    document.getElementById('content').innerHTML = htmlcontent;
 }
+
+function getContentTemplate(responseAsJson, index) {
+    const char = responseAsJson[index];
+    console.log(responseAsJson[index]);
+    
+    return `<div class="pokedex-card">
+                <h2>Name: ${responseAsJson.results[index].name}</h2>
+            </div>
+            `;
+}
+
+
+
