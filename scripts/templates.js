58e1d9003e75af5d1af0e4387e1168b8e872d3a8
DomuStyle <oxgameboyxo@googlemail.com>
2024-12-04 17:30:11 +0100

    added: loadMorePokemon(), getPokemonCardTemplate()

--- scripts/templates.js
+++ scripts/templates.js
@@ -37,8 +37,26 @@ function renderPokemonCards(pokemonArray) {
 function getShowMoreBtnTemplate() {
     let showMoreBtn = document.getElementById('show_more_btn');
     showMoreBtn.innerHTML = `
-                            <button onClick="">show more</button>
+                            <button onClick="loadMorePokemon()">show more</button>
                             `;
+}
+
+
+function getPokemonCardtemplate(pokemon) {
+    return `<div class="pokemon_card">
+                <div class="card_header">
+                    <p class="card_id"># ${pokemon.id}</p><h2 class="card_name">${pokemon.name}</h2>
+                </div>
+                <div class="card_display">
+                    <img src="${pokemon.sprites.other.home.front_default}" alt="${pokemon.name}_image">
+                </div>
+                <div class="card_body">
+                    <p><b>height:</b> ${pokemon.height}m</p>
+                    <p><b>weight:</b> ${pokemon.weight} kg</p>
+                    <p><b>Base-xp:</b> ${pokemon.base_experience}</p>
+                    <p><b>Abilities:</b> ${pokemon.abilities}</p>
+                </div>        
+            </div>`;
 }
 
 // function renderDetailsCard(){
