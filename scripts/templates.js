a8b9bc60decb2811bfed8309a483c2f09da3d77f
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-02-16 22:05:06 +0100

    refactoring: pokemon details card styling

--- scripts/templates.js
+++ scripts/templates.js
@@ -38,26 +38,28 @@ function getDetailOverlayTemplate(pokemon, index) {
     
     return `
             <div class="detail_card_border moving_gradient" onClick="childClickEvent()">
-                <div class="details_card_background" ${dynamicBackground}>
-                    <div class="overlay_header">
-                        <div class="overlay_header1">
-                            <p class="overlay_id"># ${pokemon.id}</p><h2 class="overlay_name">${pokemon.name}</h2>
+                <div class="details_card_background">
+                    <div class="details_card_display" ${dynamicBackground}>
+                        <div class="overlay_header">
+                            <div class="overlay_header1">
+                                <p class="overlay_id"># ${pokemon.id}</p><h2 class="overlay_name">${pokemon.name}</h2>
+                            </div>
+                            <div class="overlay_header2">
+                                <p class="overlay_battlepoints">BP: ${pokemon.stats[0].base_stat}</p><img class="overlay_power_img" src="" alt="poke_power_image">
+                            </div>
                         </div>
-                        <div class="overlay_header2">
-                            <p class="overlay_battlepoints">BP: ${pokemon.stats[0].base_stat}</p><img class="overlay_power_img" src="" alt="poke_power_image">
-                        </div>
-                    </div>
 
-                    <img class="overlay_img" src="${pokemon.sprites.other.home.front_default}" alt="${pokemon.name}_image">
-                </div>
-                <div class="overlay_body">
-                    <p class="overlay_body_text"><b>Height:</b> ${pokemon.height}m</p>
-                    <p class="overlay_body_text"><b>Weight:</b> ${pokemon.weight} kg</p>
-                    <p class="overlay_body_text"><b>Base XP:</b> ${pokemon.base_experience}</p>
-                    <p class="overlay_body_text"><b>Abilities:</b> ${abilityItem}</p>
-                    <div class="navigation_buttons">
-                        <button onClick="showPreviousPokemon(${index})">Previous</button>
-                        <button onClick="showNextPokemon(${index})">Next</button>
+                        <img class="overlay_img" src="${pokemon.sprites.other.home.front_default}" alt="${pokemon.name}_image">
+                    </div>
+                    <div class="overlay_body">
+                        <p class="overlay_body_text"><b>Height:</b> ${pokemon.height}m</p>
+                        <p class="overlay_body_text"><b>Weight:</b> ${pokemon.weight} kg</p>
+                        <p class="overlay_body_text"><b>Base XP:</b> ${pokemon.base_experience}</p>
+                        <p class="overlay_body_text"><b>Abilities:</b> ${abilityItem}</p>
+                        <div class="navigation_buttons">
+                            <button onClick="showPreviousPokemon(${index})">Previous</button>
+                            <button onClick="showNextPokemon(${index})">Next</button>
+                        </div>
                     </div>
                 </div>
             </div>
